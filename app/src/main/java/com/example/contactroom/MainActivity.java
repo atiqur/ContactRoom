b22bda2b12a8441840b26935e917606d8e502078
Atiqur Rahman <atiqur.india@gmail.com>
2021-08-31 14:18:32 +0530

    Adding Custom Contacts Via the Contact Activity

--- app/src/main/java/com/example/contactroom/MainActivity.java
+++ app/src/main/java/com/example/contactroom/MainActivity.java
@@ -1,19 +1,23 @@
 package com.example.contactroom;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
 import com.example.contactroom.model.Contact;
 import com.example.contactroom.model.ContactViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int NEW_CONTACT_ACTIVITY_REQUEST_CODE = 1;
     private ContactViewModel contactViewModel;
     private TextView textView;
 
@@ -34,5 +38,25 @@ public class MainActivity extends AppCompatActivity {
             }
             textView.setText(builder.toString());
         });
+
+        FloatingActionButton fab = findViewById(R.id.add_contact_fab);
+        fab.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, NewContact.class);
+            startActivityForResult(intent, NEW_CONTACT_ACTIVITY_REQUEST_CODE);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == NEW_CONTACT_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            assert data != null;
+            String name = data.getStringExtra(NewContact.NAME_REPLY);
+            String occupation = data.getStringExtra(NewContact.OCCUPATION);
+
+            assert name != null;
+            Contact contact = new Contact(name, occupation);
+            ContactViewModel.insert(contact);
+        }
     }
 }
